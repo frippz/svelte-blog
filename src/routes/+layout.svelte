da76b404425493bea3c60b7e691b30e66dddc3e6
Fredrik Frodlund <fredrik@frodlund.se>
2023-04-26 19:25:53 +0200

    feat: add base layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
+	import '../styles/all.css';
+</script>
+
+<Header />
+
+<main id="main-content">
+	<div class="landmark-content">
+		<slot />
+	</div>
+</main>
+
+<Footer />
