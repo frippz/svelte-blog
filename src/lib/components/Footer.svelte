b091171732fd308fa3e759928c32d56995a009bb
Fredrik Frodlund <fredrik@frodlund.se>
2023-04-26 19:24:53 +0200

    feat: add header and footer components

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import externalLinks from '../../data/externalLinks.json';
+	import feedLinks from '../../data/feedLinks.json';
+</script>
+
+<footer id="footer">
+	<div class="landmark-content footer-content">
+		<h2 class="offscreen">Find Fredrik in other places</h2>
+
+		<ul class="footer-links external-links">
+			{#each externalLinks as item}
+				<li>
+					<a href={item.url}>
+						{item.label}
+					</a>
+				</li>
+			{/each}
+		</ul>
+
+		<h2 class="offscreen">Subscribe to posts</h2>
+
+		<ul class="footer-links feeds">
+			{#each feedLinks as item}
+				<li>
+					<a href={item.url}>
+						{item.label}
+					</a>
+				</li>
+			{/each}
+		</ul>
+
+		<small class="copyright">© 2005–2023 Fredrik Frodlund</small>
+	</div>
+</footer>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import siteData from '../../data/site.json';
+	const site = siteData[0];
+	const navItems = [
+		{
+			url: '/',
+			label: 'Home'
+		},
+		{
+			url: '/blog/',
+			label: 'Blog'
+		},
+		{
+			url: '/about/',
+			label: 'About'
+		}
+	];
+</script>
+
+<header id="header">
+	<div class="landmark-content header-content">
+		<h1 class="main-title">
+			<a href="/" aria-label={site.titlePhonetic}>{site.title}</a>
+		</h1>
+		<nav>
+			<ul id="main-nav">
+				{#each navItems as { url, label }}
+					<li><a href={url}>{label}</a></li>
+				{/each}
+			</ul>
+		</nav>
+	</div>
+</header>
